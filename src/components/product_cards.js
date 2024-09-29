06c9cb4718dc2c9f60bca675e0bc161dec3e194b
ufukincibas <ufukincibas@hotmail.com>
2024-09-29 10:32:48 +0300

    second commit

--- src/components/product_cards.js
+++ src/components/product_cards.js
@@ -5,11 +5,22 @@ import styles from './styles'
 const Card = (props) => {
     return(
         <View style={styles.container}>
+        <View style={styles.inner_container}>
+            <View style={styles.deneme}>
+                     <View style={styles.top}>
                 
-                     <Image style={styles.image} src={props.products.imgURL}/>
-               
+                        <Image style={styles.image} src={props.products.imgURL}/>
+                     </View>
+               <View style={styles.bottom}>
             <Text style={styles.title}>{props.products.title}</Text>
             <Text style={styles.price}>{props.products.price}</Text>
+            {!props.products.inStock && (
+                        <Text style={styles.inStock}>STOKTA YOK</Text>
+                    )}
+                    </View>
+            </View>
+           
+        </View>
         </View>
     )
 }
--- src/components/styles.js
+++ src/components/styles.js
@@ -1,13 +1,29 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet ,Dimensions} from "react-native";
 
 export default StyleSheet.create({
-    container:{ backgroundColor:'gray',
-        flex:1,
-        alignItems:'center',
-        justifyContent:'center',
-        padding:10
+    container: {backgroundColor: "#f5f5f5",
+    margin: 7,
+    borderRadius: 10,
+    marginLeft: 0,
+    width:180,
+    flex:2,
+    flexDirection:'column'
     },
-    innercontainer:{backgroundColor:'white'},
+
+  
+    inner_container:{  marginTop: 10,
+        marginBottom: 5,
+        alignItems: "center",
+        flex:1,
+         flexDirection:'row'
+        
+
+    },
+    top:{  marginLeft: 5,
+        marginRight:5},
+        bottom: {
+            marginTop:3,
+            marginBottom:4},
     title: {fontSize:20 ,
         fontWeight:'bold',
         marginBottom:10,
@@ -16,10 +32,16 @@ export default StyleSheet.create({
     price:{fontSize:16,
       
     },
-    image : {width:50,
-        height:50,
-        backgroundColor:'white'
+    image : {borderRadius: 10,
+        width: 160,
+        height: 180,
+       
     },
+    inStock: {
+        fontWeight: 'bold',
+        fontSize: 12,
+        color: 'red'
+    }
 
 
 
